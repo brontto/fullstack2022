e46d2387a911eedd325cf6c58a2c66a5681d996d
Henri Virtanen <bronttosaur@gmail.com>
2022-06-29 12:06:48 +0300

    4.8

---
+++ osa4/blogilista/tests/test_helper.js
@@ -0,0 +1,34 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "HuippuBlogi",
+        author: "Jaska Pekkanen",
+        url: "www.jaskanblogi.org",
+        likes: 6
+    },
+    {
+        title: "Autoblogi",
+        author: "Pekka Automies",
+        url: "www.autotonkivoja.fi",
+        likes: 0
+    }
+]
+
+
+const nonExistingId = async () => {
+    const blog = new Blog({title: "title", url: "urli tähän"})
+    await blog.save()
+    await blog.remove()
+    return blog._id.toString()
+
+}
+
+const blogsInDb = async () => {
+    const blogs = await Blogs.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+    initialBlogs, nonExistingId, blogsInDb
+}
