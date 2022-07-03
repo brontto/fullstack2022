3f989a063d0e3b4b111d43e7175a23159163def2
Henri Virtanen <bronttosaur@gmail.com>
2022-07-03 20:10:48 +0300

    4.17

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -6,13 +6,13 @@ userRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
 
     const existingUser = await User.findOne({ username })
-    if(existingUser) {
+    if (existingUser) {
         return response.status(400).json({
             error: 'username must be unique'
         })
     }
 
-    if(!password || password.length < 3){
+    if (!password || password.length < 3) {
         return response.status(400).json({
             error: 'password must be at least 3 characters long'
         })
@@ -34,7 +34,7 @@ userRouter.post('/', async (request, response) => {
 
 userRouter.get('/', async (request, response) => {
     const users = await User
-        .find({})
+        .find({}).populate('blogs', { url: 1, title: 1, author: 1 })
 
     response.json(users)
 })
