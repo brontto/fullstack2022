82adfe9ed28396b3efb63afc8ad26435479b4a71
Henri Virtanen <bronttosaur@gmail.com>
2022-09-24 11:59:19 +0300

    tehtävä 5.2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -18,6 +18,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -26,6 +35,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -33,6 +45,11 @@ const App = () => {
       console.log(exception)
     }
 
+  }
+
+  const logout = () => { 
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   const page = () => {
@@ -68,6 +85,7 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
+        <p>{user.name} logged in <button onClick={logout}>logout</button></p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
