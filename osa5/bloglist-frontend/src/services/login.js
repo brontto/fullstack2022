d7b5edd03e3bfeeec48c9562a93a8ed97eed0fc4
Henri Virtanen <bronttosaur@gmail.com>
2022-09-24 11:49:59 +0300

    tehtävä 5.1

---
+++ osa5/bloglist-frontend/src/services/login.js
@@ -0,0 +1,10 @@
+//login service for login and logout with axios 
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+const login = async credentials => {
+    const response = await axios.post(baseUrl, credentials)
+    return response.data
+}
+
+export default { login }
