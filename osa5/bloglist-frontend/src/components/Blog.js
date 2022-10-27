1eb1e686fd44aca4761ed92c7f38a688142b1dcd
Henri Virtanen <bronttosaur@gmail.com>
2022-10-27 23:55:31 +0300

    5.7 yksittäiselle blogille nappi + css judui

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    width: 300
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  const hideWhenVisible = { display: showDetails ? 'none' : '' }
+  const showWhenVisible = { display: showDetails ? '' : 'none' }
+
+
+  return (
+    <div style={blogStyle}>
+
+      <div style={hideWhenVisible}>
+        <div onClick={() => setShowDetails(true)} style={{cursor: 'pointer'}}>{blog.title}, {blog.author}</div>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title}, {blog.author}
+        <button onClick={() => setShowDetails(false)}>hide</button>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default Blog
