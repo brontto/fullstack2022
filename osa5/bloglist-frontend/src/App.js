610534ae488f6b5e8d771514c51e13b7f10f04d1
Henri Virtanen <bronttosaur@gmail.com>
2022-12-02 21:45:34 +0200

    5.9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -114,7 +114,8 @@ const App = () => {
           addBlog={addBlog}
         />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes)
+        .map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog}/>
       )}
     </div>
