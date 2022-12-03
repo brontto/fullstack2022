a0cf276b295d8b5d057e7550d6964f79b18c1eeb
Henri Virtanen <bronttosaur@gmail.com>
2022-12-03 19:00:39 +0200

    5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -4,39 +4,60 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'blog title',
-    author: 'blog author',
-    url: 'www.blog.com'
-  }
+describe('<Blog />', () => {
+  let blog
 
-  const mockHandler = jest.fn()
+  beforeEach(() => {
+    blog = {
+      title: 'blog title',
+      author: 'blog author',
+      url: 'www.blog.com',
+      likes: 0
+    }
+  })
 
-  render(<Blog blog={blog} removeBlog={mockHandler} updateBlog={mockHandler} />)
+  test('renders content', () => {
 
-  const element = screen.getByPlaceholderText('sort version')
-  expect(element).toHaveTextContent('blog title, blog author')
-})
+    const mockHandler = jest.fn()
 
-test('clicking the title opens details', async () => {
-  const blog = {
-    title: 'blog title',
-    author: 'blog author',
-    url: 'www.blog.com',
-    likes: 0
-  }
+    render(<Blog blog={blog} removeBlog={mockHandler} updateBlog={mockHandler} />)
 
-  const mockHandler = jest.fn()
+    const element = screen.getByPlaceholderText('sort version')
+    expect(element).toHaveTextContent('blog title, blog author')
+  })
 
-  render(<Blog blog={blog} removeBlog={mockHandler} updateBlog={mockHandler} />)
+  test('clicking the title opens details', async () => {
 
-  const user = userEvent.setup()
-  const button = screen.getByPlaceholderText('sort version')
-  await user.click(button)
+    const mockHandler = jest.fn()
 
-  const element = screen.getByPlaceholderText('long version')
-  expect(element).toHaveTextContent('www.blog.com')
-  expect(element).toHaveTextContent('likes 0')
-  expect(element).toHaveTextContent('blog author')
+    render(<Blog blog={blog} removeBlog={mockHandler} updateBlog={mockHandler} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByPlaceholderText('sort version')
+    await user.click(button)
+
+    const element = screen.getByPlaceholderText('long version')
+    expect(element).toHaveTextContent('www.blog.com')
+    expect(element).toHaveTextContent('likes 0')
+    expect(element).toHaveTextContent('blog author')
+  })
+
+
+  test('clicking the like button twice calls event handler twice', async () => {
+      
+      const mockHandler = jest.fn()
+  
+      render(<Blog blog={blog} removeBlog={mockHandler} updateBlog={mockHandler} />)
+  
+      const user = userEvent.setup()
+      const button = screen.getByPlaceholderText('sort version')
+      await user.click(button)
+  
+      const likeButton = screen.getByText('like')
+      await user.click(likeButton)
+      await user.click(likeButton)
+  
+      expect(mockHandler.mock.calls).toHaveLength(2)
+  })
+
 })
