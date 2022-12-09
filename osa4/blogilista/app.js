d7c20ff3d4e552ab58c3fef4bbab7ba8eb6ec4ca
Henri Virtanen <bronttosaur@gmail.com>
2022-12-10 00:36:33 +0200

    5.17

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -9,7 +9,7 @@ const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-
+const testingRouter = require('./controllers/testing')
 
 logger.info('connection to', config.MONGODB_URI)
 
@@ -30,6 +30,12 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
